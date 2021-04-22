0e44bdf9d68c1df505a5b90b4b072bbeedb338f6
Luiz Antunes <luiz.antunes@codificar.com.br>
2021-04-22 00:21:12 -0300

    iniciando repositório

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,54 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/scorecard/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget() : super(
+    preferredSize: Size.fromHeight(250),
+    child: Container(
+      height: 250,
+      child: Stack(
+        children: [
+          Container(
+            height: 161,
+            width: double.maxFinite,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(gradient: AppGradients.linear),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Olá, ",
+                    style: AppTextStyles.title,
+                    children: [
+                      TextSpan(
+                        text: "Luiz Antunes",
+                        style: AppTextStyles.titleBold
+                      )
+                    ]
+                  )
+                ),
+                Container(
+                  width: 58,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage("https://media.gazetadopovo.com.br/2020/10/19192426/gata-tem-covid-diagnosticada-660x372.jpg")
+                    )
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment(0.0, 1.0),
+            child: ScoreCardWidget()
+          )
+        ],
+      ),
+    )
+  );
+
+}
